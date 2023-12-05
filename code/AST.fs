0bb4d1334731c1f7f837db7033f7b9dbb4c32d8c
Ben Wilen <bcw3@williams.edu>
2023-12-05 00:40:14 -0500

    Update

--- code/AST.fs
+++ code/AST.fs
@@ -9,6 +9,7 @@ type Expr =
 | Furniture of attrs: Expr list
 | Room of attrs: Expr list * children: Expr list
 | Level of attrs: Expr list * children: Expr list
-| TypeDef of var: Expr * pars: Expr list * children: Expr list
-| Assignment of Expr //should be a typeDef
+| TypeDef of pars: Expr list * children: Expr list
+| TypeInstance of var: Expr * pars: Expr list * children: Expr list
+| Assignment of Expr * Expr
 | Sequence of Expr list
